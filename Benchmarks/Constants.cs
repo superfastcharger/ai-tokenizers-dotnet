33473a15e5d94b5dcf924239a3d81e6db874526a
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2022-01-03 09:48:44 +0000

    fixed bad link

--- Benchmarks/Constants.cs
+++ Benchmarks/Constants.cs
@@ -200,3 +200,49 @@ Root domain: example.com
 ✅ https://foo.example.com/.well-known/ai-plugin.json → redirects to https://bar.foo.example.com/.well-known/ai-plugin.json
 Root domain: bar.foo.example.com
 ✅ https://foo.example.com/.well-known/ai-plugin.json → redirects to https://bar.foo.example.com/baz/ai-plugin.json
+Root domain: bar.foo.example.com
+❌ https://foo.example.com/.well-known/ai-plugin.json → redirects to https://example.com/.well-known/ai-plugin.json
+Redirect to parent level domain is disallowed
+❌ https://foo.example.com/.well-known/ai-plugin.json → redirects to https://bar.example.com/.well-known/ai-plugin.json
+Redirect to same level subdomain is disallowed
+❌ https://example.com/.well-known/ai-plugin.json -> redirects to https://example2.com/.well-known/ai-plugin.json
+Redirect to another domain is disallowed
+Manifest validation
+Specific fields in the manifest itself must satisfy the following requirements:
+
+api.url - the URL provided to the OpenAPI spec must be hosted at the same level or a subdomain of the root domain.
+legal_info - The second-level domain of the URL provided must be the same as the second-level domain of the root domain.
+contact_info - The second-level domain of the email address should be the same as the second-level domain of the root domain.
+Resolving the API spec
+The api.url field in the manifest provides a link to an OpenAPI spec that defines APIs that the plugin can call into. OpenAPI allows specifying multiple server base URLs. The following logic is used to select the server URL:
+
+Iterate through the list of server URLs
+Use the first server URL that is either an exact match of the root domain or a subdomain of the root domain
+If neither cases above apply, then default to the domain where the API spec is hosted. For example, if the spec is hosted on api.example.com, then api.example.com will be used as the base URL for the routes in the OpenAPI spec.
+Note: Please avoid using redirects for hosting the API spec and any API endpoints, as it is not guaranteed that redirects will always be followed.
+
+Use TLS and HTTPS
+All traffic with the plugin (e.g., fetching the ai-plugin.json file, the OpenAPI spec, API calls) must use TLS 1.2 or later on port 443 with a valid public certificate.
+
+IP egress ranges
+ChatGPT will call your plugin from an IP address in the CIDR block 23.102.140.112/28. You may wish to explicitly allowlist these IP addresses.
+
+Separately, OpenAI's web browsing plugin accesses websites from a different IP address block: 23.98.142.176/28.
+
+FAQ
+How is plugin data used?
+Plugins connect ChatGPT to external apps. If a user enables a plugin, ChatGPT may send parts of their conversation and their country or state to your plugin.
+
+What happens if a request to my API fails?
+If an API request fails, the model might retry the request up to 10 times before letting the user know it cannot get a response from that plugin.
+
+Can I invite people to try my plugin?
+Yes, all unverified plugins can be installed by up to 100 other developers who have plugin access. If your plugin is available in the plugin store, it will be accessible to all ChatGPT plus customers.
+
+Can I charge people money for my plugin?
+Not at this time.
+";
+        internal const string AlgebraEquation = @"anxn + an−1xn−1 + … + a2x2 + a1x + a0 = 0,";
+        internal const string UnicodeCharacters = "こんにちは世界こんにちは世界こんにちは世界こんにちは世界こんにちは世界こんにちは世界こんにちは世界こんにちは世界こんにちは世界こんにちは世界こんにちは世界こんにちは世界";
+    }
+}
