de23e35d9e95d3fe86841d6bb011ce5948cc755a
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2022-09-20 08:07:57 +0000

    Added solutions

--- LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
+++ LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
@@ -35,4 +35,4 @@ namespace LLMSharp.Anthropic.Tokenizer
         /// Special tokens are artificial tokens used to unlock capabilities from a model,
         /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
         /// tokens, since they can be used to trick a model into doing something we don't want it to do.
-        /// This method uses the default imple
+        /// This method uses the default implementation and throws an error if the text contains any valid special
