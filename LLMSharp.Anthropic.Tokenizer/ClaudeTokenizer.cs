43e1fc0d062ad14b4d108cc4df04e6628b730f47
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-02-02 08:37:48 +0000

    password reset

--- LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
+++ LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
@@ -64,4 +64,5 @@ namespace LLMSharp.Anthropic.Tokenizer
         /// Encodes a string into tokens using claude bpe ranks
         /// Special tokens are artificial tokens used to unlock capabilities from a model,
         /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
-        /// tokens, since they can be used to trick a model into doing something we don't want it to do
+        /// tokens, since they can be used to trick a model into doing something we don't want it to do.
+        /// 1. If the tokenizer needs to allow all special toke
