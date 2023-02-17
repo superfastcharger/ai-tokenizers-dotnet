8ae7ab859fb27ab70c469ce75214b718b5202327
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-02-17 18:02:03 +0000

    [IMP] typo

--- LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
+++ LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
@@ -70,4 +70,5 @@ namespace LLMSharp.Anthropic.Tokenizer
         /// 3. allowedSpecialTokens and disallowedSpecialTokens should contain only valid supported tokens by the model
         /// </summary>
         /// <param name="text">text to encode using claude tokenizer</param>
-        /// <param name="allowedSpecialTokens">special tokens that are allowed for tokenization. If null, all the special tokens supported by the model are allowed. If empty, none of the special tokens a
+        /// <param name="allowedSpecialTokens">special tokens that are allowed for tokenization. If null, all the special tokens supported by the model are allowed. If empty, none of the special tokens are allowed.</param>
+        /// <param name="disallowedSpecial
