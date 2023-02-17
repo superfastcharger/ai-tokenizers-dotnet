444acf908c92e256536c674d9f0683ebfc8d33bb
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-02-17 12:45:28 +0000

    removed a print statement

--- LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
+++ LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
@@ -70,4 +70,4 @@ namespace LLMSharp.Anthropic.Tokenizer
         /// 3. allowedSpecialTokens and disallowedSpecialTokens should contain only valid supported tokens by the model
         /// </summary>
         /// <param name="text">text to encode using claude tokenizer</param>
-        /// <param name="allowedSpecialTokens">special to
+        /// <param name="allowedSpecialTokens">special tokens that are allowed for tokenization. If null, all the special tokens supp
