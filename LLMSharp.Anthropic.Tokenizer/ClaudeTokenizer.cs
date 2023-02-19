6036c6996edb5d04a0dc8a11eac64eee6095cd44
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-02-19 18:09:01 +0000

    Quick Fix

--- LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
+++ LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
@@ -72,4 +72,5 @@ namespace LLMSharp.Anthropic.Tokenizer
         /// <param name="text">text to encode using claude tokenizer</param>
         /// <param name="allowedSpecialTokens">special tokens that are allowed for tokenization. If null, all the special tokens supported by the model are allowed. If empty, none of the special tokens are allowed.</param>
         /// <param name="disallowedSpecialTokens">special tokens that should be disallowed for tokenization. If null, any special token that is not allowed will be considered disallowed.</param>
-        /// <returns>encoded tokens list of in
+        /// <returns>encoded tokens list of input text</returns>
+        public IReadOnlyList<int> EncodeWithSpecialToken
