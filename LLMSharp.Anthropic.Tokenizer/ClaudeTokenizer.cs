313e4b378d252d162dcde2367a736c022c6c641b
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-03-22 11:14:38 +0000

    fixed dates

--- LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
+++ LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
@@ -84,4 +84,5 @@ namespace LLMSharp.Anthropic.Tokenizer
         /// <summary>
         /// Counts number of byte pair encoded tokens for the given text input
         /// Special tokens are artificial tokens used to unlock capabilities from a model,
-        /// such as fill-in-the-middle.So we want to be c
+        /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
+        /// tokens, since they ca
