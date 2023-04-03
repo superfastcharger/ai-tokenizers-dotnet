e102b1fbd8f08c6b324f76384e2a2bf1256a703a
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-04-03 22:31:40 +0000

    Reviewed by Oliver Hunt.

--- LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
+++ LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
@@ -91,4 +91,5 @@ namespace LLMSharp.Anthropic.Tokenizer
         /// 3. allowedSpecialTokens and disallowedSpecialTokens should contain only valid supported tokens by the model
         /// </summary>
         /// <param name="text">text input for counting tokens using claude tokenizer</param>
-        /// <param name="allowedSpecialTokens">special tokens that are allowed for tokenization. If null, all the special tokens supported by the model are allowed. If empty, none of the special tokens are allowed.</pa
+        /// <param name="allowedSpecialTokens">special tokens that are allowed for tokenization. If null, all the special tokens supported by the model are allowed. If empty, none of the special tokens are allowed.</param>
+        /// <param name="disallowedSpecialTokens">special tokens that should
