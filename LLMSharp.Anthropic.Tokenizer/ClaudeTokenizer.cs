fc67d63e4240f16fbf5414ed1d7f821fadb9168d
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-04-12 22:23:54 +0000

    :art: cleanup

--- LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
+++ LLMSharp.Anthropic.Tokenizer/ClaudeTokenizer.cs
@@ -94,4 +94,5 @@ namespace LLMSharp.Anthropic.Tokenizer
         /// <param name="allowedSpecialTokens">special tokens that are allowed for tokenization. If null, all the special tokens supported by the model are allowed. If empty, none of the special tokens are allowed.</param>
         /// <param name="disallowedSpecialTokens">special tokens that should be disallowed for tokenization. If null, any special token that is not allowed will be considered disallowed.</param>
         /// <returns>number of tokens for the given text</returns>
-        /// <exception cref="InvalidOperationException">thrown when any of the disallowed special tokens are found in the text</exceptio
+        /// <exception cref="InvalidOperationException">thrown when any of the disallowed special tokens are found in the text</exception>
+        public int CountWithSpecialTokens(string text, IEnumerable<
