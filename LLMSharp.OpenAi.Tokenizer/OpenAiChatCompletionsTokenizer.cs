6646e578193ce90b9caf725eff3f8a79f0db9f5a
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-06-13 06:26:43 +0000

    typo..

--- LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
+++ LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
@@ -49,4 +49,4 @@ namespace LLMSharp.OpenAi.Tokenizer
         /// Counts number of byte pair encoded tokens for the given text input
         /// Special tokens are artificial tokens used to unlock capabilities from a model,
         /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
-   
+        /// tokens, since they can be used to trick a model into do
