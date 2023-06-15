9d350ff80885a96d65930eda6fba2207e4d75e6f
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-06-15 12:15:29 +0000

    Fixed language

--- LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
+++ LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
@@ -50,4 +50,5 @@ namespace LLMSharp.OpenAi.Tokenizer
         /// Special tokens are artificial tokens used to unlock capabilities from a model,
         /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
         /// tokens, since they can be used to trick a model into doing something we don't want it to do.
-        /// This method uses the default implementation and throws an error if the text contains any
+        /// This method uses the default implementation and throws an error if the text contains any valid special tokens.
+        /// For more granular control of counting 
