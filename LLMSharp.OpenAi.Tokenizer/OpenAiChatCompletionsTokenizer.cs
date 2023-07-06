c16975600b9dad51230f6d16186c7a5f7da02438
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-07-06 10:39:29 +0000

    UPD: readme

--- LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
+++ LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
@@ -70,4 +70,4 @@ namespace LLMSharp.OpenAi.Tokenizer
         /// 2. If the tokenizer needs to allow only a limited set of special tokens : use the allowedSpecialTokens for allowed and disallowedSpecialTokens for tokens to be disallowed
         /// 3. allowedSpecialTokens and disallowedSpecialTokens should contain only valid supported tokens by the model
         /// </summary>
-        /// <pa
+        /// <param name="text">text to encode using openai chat completions tok
