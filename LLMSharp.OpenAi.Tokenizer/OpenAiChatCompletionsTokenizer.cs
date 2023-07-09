ca932312e22e3ba038ee550123f0fcdfa543efb5
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-07-09 15:04:09 +0000

    merging staging into dev

--- LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
+++ LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
@@ -73,4 +73,5 @@ namespace LLMSharp.OpenAi.Tokenizer
         /// <param name="text">text to encode using openai chat completions tokenizer</param>
         /// <param name="allowedSpecialTokens">special tokens that are allowed for tokenization. If null, all the special tokens supported by the model are allowed. If empty, none of the special tokens are allowed.</param>
         /// <param name="disallowedSpecialTokens">special tokens that should be disallowed for tokenization. If null, any special token that is not allowed will be considered disallowed.</param>
-        /// <returns>number of tokens for the given text</re
+        /// <returns>number of tokens for the given text</returns>
+        /// <exception cref="InvalidOperationException">th
