0acfea1179e4226586e380922c4252b3e639ad67
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-07-19 19:45:38 +0000

    Update test.rb

--- LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
+++ LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
@@ -89,4 +89,5 @@ namespace LLMSharp.OpenAi.Tokenizer
         /// Special tokens are artificial tokens used to unlock capabilities from a model,
         /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
         /// tokens, since they can be used to trick a model into doing something we don't want it to do.
-        /// 1. If the tokenizer needs to allow all special tokens : pass n
+        /// 1. If the tokenizer needs to allow all special tokens : pass null for allowedSpecialTokens and disallowedSpecialTokens
+       
