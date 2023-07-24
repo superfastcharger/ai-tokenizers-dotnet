671c3c30b125d6d78dd06c05ddf8bd07e05e19ff
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-07-24 15:24:16 +0000

    USB: serial: fix potential use-after-free after failed probe

--- LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
+++ LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
@@ -93,4 +93,5 @@ namespace LLMSharp.OpenAi.Tokenizer
         /// 2. If the tokenizer needs to allow only a limited set of special tokens : use the allowedSpecialTokens for allowed and disallowedSpecialTokens for tokens to be disallowed
         /// 3. allowedSpecialTokens and disallowedSpecialTokens should contain only valid supported tokens by the model
         /// </summary>
-        /// <param name="text">text input for counting token
+        /// <param name="text">text input for counting tokens using gpt chatcompletions tokenizer</param>
+        /// <param na
