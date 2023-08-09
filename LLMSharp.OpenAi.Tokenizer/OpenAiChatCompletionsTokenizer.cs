56d87eb5a400916adb027955fe223b33bb569f41
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-08-09 18:55:36 +0000

    initial commit comment

--- LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
+++ LLMSharp.OpenAi.Tokenizer/OpenAiChatCompletionsTokenizer.cs
@@ -97,4 +97,4 @@ namespace LLMSharp.OpenAi.Tokenizer
         /// <param name="allowedSpecialTokens">special tokens that are allowed for tokenization. If null, all the special tokens supported by the model are allowed. If empty, none of the special tokens are allowed.</param>
         /// <param name="disallowedSpecialTokens">special tokens that should be disallowed for tokenization. If null, any special token that is not allowed will be considered disallowed.</param>
         /// <returns>number of tokens for the given text</returns>
-        ///
+        /// <exception cref="InvalidOperationException">thrown when any
