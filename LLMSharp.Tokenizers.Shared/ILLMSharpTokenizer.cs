42145ae6bfdf60d753b18214fbe3f698a2e9a5f5
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-10-28 12:01:53 +0000

    Added lib

--- LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
+++ LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
@@ -3,4 +3,6 @@ using System.Collections.Generic;
 namespace LLMSharp.Tokenizers.Shared
 {
     /// <summary>
-    /// A common interface definition for any LLMSharpTokenizer to
+    /// A common interface definition for any LLMSharpTokenizer to implement
+    /// </summary>
+    public interface ILLMSharpToken
