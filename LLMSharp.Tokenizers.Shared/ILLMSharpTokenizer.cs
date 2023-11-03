85114dea6b764c866357df55d70821f1e584a1b4
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-11-03 10:09:05 +0000

    Add change log.

--- LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
+++ LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
@@ -11,4 +11,5 @@ namespace LLMSharp.Tokenizers.Shared
         /// Byte Pair Encodes a string into tokens
         /// Special tokens are artificial tokens used to unlock capabilities from a model,
         /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
-        /// tokens, since they can be used to trick a 
+        /// tokens, since they can be used to trick a model into doing something we don't want it to do.
+        /// encode w
