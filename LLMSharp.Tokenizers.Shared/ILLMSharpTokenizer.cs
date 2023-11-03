6704ebfae2fb01e47978469c2d92dfcf8ec18364
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-11-03 12:10:11 +0000

    Removed obsolete file.

--- LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
+++ LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
@@ -12,4 +12,5 @@ namespace LLMSharp.Tokenizers.Shared
         /// Special tokens are artificial tokens used to unlock capabilities from a model,
         /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
         /// tokens, since they can be used to trick a model into doing something we don't want it to do.
-        /// encode w
+        /// encode will raise an error if it encounters text that corresponds
+        /// to a spe
