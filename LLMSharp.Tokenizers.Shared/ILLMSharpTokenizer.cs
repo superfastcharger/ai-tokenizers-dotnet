4ab8ab9f1ef57fc436d16b6576fa71bed93b5838
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-11-09 22:37:44 +0000

    drm/vmwgfx: Fix a case where the code would BUG when trying to pin GMR memory

--- LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
+++ LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
@@ -25,4 +25,5 @@ namespace LLMSharp.Tokenizers.Shared
         /// Special tokens are artificial tokens used to unlock capabilities from a model,
         /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
         /// tokens, since they can be used to trick a model into doing something we don't want it to do.
+        /// CountTokens will raise an error if it encounters text that corresponds
   
