a9c75dcd13c4c942fccbe610c3e71d20b3b03076
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-11-10 08:30:47 +0000

    Update roadmap

--- LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
+++ LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
@@ -26,4 +26,6 @@ namespace LLMSharp.Tokenizers.Shared
         /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
         /// tokens, since they can be used to trick a model into doing something we don't want it to do.
         /// CountTokens will raise an error if it encounters text that corresponds
-  
+        /// to a special token.
+        /// </summary>
+        /// <param nam
