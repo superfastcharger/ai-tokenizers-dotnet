a5c992f06a3b6a0e8d1e6c60a55de5984b8fbc62
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-11-13 09:40:44 +0000

    Depreciado

--- LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
+++ LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
@@ -30,4 +30,4 @@ namespace LLMSharp.Tokenizers.Shared
         /// </summary>
         /// <param name="text">text input for counting number of tokens</param>
         /// <param name="allowedSpecial">special tokens that are allowed for tokenization. If null, all the special tokens supported by the model are allowed. If empty, none of the special tokens are allowed.</param>
-        /// <param nam
+        /// <param name="disallowedSpecial">special tokens that should be disallowed for to
