094568aa0f341a05f508823c4b1099c3053658ba
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-11-24 22:38:38 +0000

    * Added test code of CSS for h3 tag.

--- LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
+++ LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
@@ -39,4 +39,5 @@ namespace LLMSharp.Tokenizers.Shared
         /// Byte Pair Encodes a string into tokens
         /// Special tokens are artificial tokens used to unlock capabilities from a model,
         /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
-        /// tokens, since they can be used to trick a model into doing something we don't w
+        /// tokens, since they can be used to trick a model into doing something we don't want it to do.
+        /// Hence, by default, encode will ra
