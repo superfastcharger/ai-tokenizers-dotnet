52b6eaf05544f3800d75d50c7372b4ef18e1dd88
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-11-26 17:27:40 +0000

    Updated French doc translation

--- LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
+++ LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
@@ -43,4 +43,4 @@ namespace LLMSharp.Tokenizers.Shared
         /// Hence, by default, encode will raise an error if it encounters text that corresponds
         /// to a special token.This can be controlled on a per-token level using the `allowed_special`
         /// and `disallowed_special` parameters.In particular:
-   
+        /// Setting 'disallowedSpecial' to null will prevent this fu
