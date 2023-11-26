0edaec09c6cbeaf58872c0f0a2ca673a21394d3b
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-11-26 15:12:25 +0000

    Delete old code

--- LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
+++ LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
@@ -41,4 +41,6 @@ namespace LLMSharp.Tokenizers.Shared
         /// such as fill-in-the-middle.So we want to be careful about accidentally encoding special
         /// tokens, since they can be used to trick a model into doing something we don't want it to do.
         /// Hence, by default, encode will raise an error if it encounters text that corresponds
-        /// to a special token.This can be controlled on a per-token level using the `allowed_spe
+        /// to a special token.This can be controlled on a per-token level using the `allowed_special`
+        /// and `disallowed_special` parameters.In particular:
+   
