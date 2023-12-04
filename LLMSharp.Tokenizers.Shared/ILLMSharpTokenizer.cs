45b46a364b075aeb3f7aebd490c854e141eb7480
superfastcharger <144878448+superfastcharger@users.noreply.github.com>
2023-12-04 22:15:54 +0000

    Another small fix.

--- LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
+++ LLMSharp.Tokenizers.Shared/ILLMSharpTokenizer.cs
@@ -49,4 +49,4 @@ namespace LLMSharp.Tokenizers.Shared
         /// corresponding to special tokens to be encoded as special tokens.
         /// </summary>
         /// <param name="text">text input for counting number of tokens</param>
-        /// <param name="allowedSpecial">special tokens that are allowed for tokenization. If null, all the special tokens supported by the mo
+        /// <param name="allowedSpecial">special tokens that are allowed for tokenization. If null, all the special tokens supported by the model are allowed. If empty, none of the special tokens are allowed.</
